416be477c75ee5f30e8ac909f2ca86d1ac11b47b
Stéphane MEYER <stm3y3r@icloud.com>
2022-06-20 14:21:11 +0200

    improved with views and a function

--- 1_persistance/3_composants_acces/1_interroger/ecole.sql
+++ 1_persistance/3_composants_acces/1_interroger/ecole.sql
@@ -9,6 +9,7 @@ create table etudiant (
   nom char(8) not null default upper(left(md5(rand()), 8)) unique
 );
 
+-- ajout de 15 étudiants.
 insert into etudiant (id) values
 (null),
 (null),
@@ -32,6 +33,7 @@ create table matiere (
   coeff int not null default (floor(rand()*5)+1)
 );
 
+-- ajout de 5 matières.
 insert into matiere (id) values
 (null),
 (null),
@@ -50,17 +52,67 @@ create table eval (
   foreign key (id_matiere) references matiere(id)
 );
 
-insert into eval (id_etudiant, id_matiere)
-select etudiant.id, matiere.id from etudiant, matiere;
+-- ajout de 5 notes par matière pour chaque élève.
+delimiter //
 
-insert into eval (id_etudiant, id_matiere)
-select etudiant.id, matiere.id from etudiant, matiere;
+create procedure ajout_notes()
 
-insert into eval (id_etudiant, id_matiere)
-select etudiant.id, matiere.id from etudiant, matiere;
+begin
+  declare iter int;
+  set iter = 10;
 
-insert into eval (id_etudiant, id_matiere)
-select etudiant.id, matiere.id from etudiant, matiere;
+  theloop: while iter > 0 do
+    insert into eval (id_etudiant, id_matiere)
+    select etudiant.id, matiere.id from etudiant, matiere;
+    set iter = iter - 1;
+  end while theloop;
+end //
 
-insert into eval (id_etudiant, id_matiere)
-select etudiant.id, matiere.id from etudiant, matiere;
+delimiter ;
+
+call ajout_notes;
+
+drop procedure ajout_notes;
+
+-- affiche la moyenne de chaque élève par matière.
+create view moyenne_eleve_matiere as
+select nom, libelle, avg(note) as moyenne
+from eval
+join matiere on eval.id_matiere = matiere.id
+join etudiant on eval.id_etudiant = etudiant.id
+group by etudiant.id, matiere.id;
+
+-- affiche la moyenne générale pondérée de chaque éléve.
+create view moyenne_generale_ponderee_eleve as
+select nom, sum(note*coeff)/sum(coeff) as moyenne_generale_ponderee
+from eval
+join matiere on eval.id_matiere = matiere.id
+join etudiant on eval.id_etudiant = etudiant.id
+group by eval.id_etudiant
+order by nom;
+
+delimiter O_o
+
+-- retourne la moyenne générale pondérée.
+create function moyenne_generale_ponderee() returns float unsigned
+begin
+  return (
+    select (sum(note*coeff)/sum(coeff))
+    from eval
+    join matiere on eval.id_matiere = matiere.id
+  );
+end O_o
+
+delimiter ;
+
+delimiter o_O
+
+-- affiche les élèves dont la moyenne générale pondérée est supérieure à la moyenne générale générale.
+create view moyenne_eleve_sup_moyenne_gen as
+select nom, sum(note*coeff)/sum(coeff) as moyenne_generale, moyenne_generale_ponderee() as mgp
+from eval
+join matiere on eval.id_matiere = matiere.id
+join etudiant on eval.id_etudiant = etudiant.id
+group by eval.id_etudiant
+having moyenne_generale >= mgp
+order by moyenne_generale desc;
