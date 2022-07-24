cbb6a7031c83ccecd819655efcb29fb6b43d789d
Stéphane MEYER <tigerlost@me.com>
2022-07-24 10:24:19 +0200

    moved keynote in its own repo.

--- B_keynote/blog_app/myblog/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import IndexView, ArticleView
-
-urlpatterns = [
-  path('', IndexView.as_view(), name='index'),
-  path('article/<int:pk>', ArticleView.as_view(), name='detail'),
-]
--- B_keynote/blog_app/myblog/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-
-set -e
-
-./manage.py makemigrations --no-input
-./manage.py migrate --no-input
-./manage.py collectstatic --no-input
-
-gunicorn myblog.wsgi:application --bind "0.0.0.0:8080"
